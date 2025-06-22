eda58a54c73344f06996271ef644cc9806dd50f5
Ceball0s <ceballosmiguel44@gmail.com>
2025-06-22 15:13:39 -0500

    Conexion todos los microservicios

---
+++ src/main/java/com/ApiGateway/JwtAuthFilter.java
@@ -0,0 +1,55 @@
+package com.ApiGateway;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthFilter implements GlobalFilter, Ordered {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            try {
+                Claims claims = Jwts.parser()
+                        .setSigningKey(secretKey.getBytes())
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                String userId = claims.getSubject(); // El "sub" debe ser el ID de usuario
+
+                ServerHttpRequest mutatedRequest = exchange.getRequest()
+                        .mutate()
+                        .header("user-id", userId)
+                        .build();
+
+                return chain.filter(exchange.mutate().request(mutatedRequest).build());
+            } catch (Exception e) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+        }
+
+        // Si no hay token, continuar sin agregar user-id
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; // Alta prioridad
+    }
+}
